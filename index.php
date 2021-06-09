4ff8b024e716b7fc678d8194222909002f9e10db
philippe-blanquier <philippe.blanquier@gmail.com>
2021-06-09 10:25:34 +0200

    creation

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+        <title>PHP ex2</title>
+    </head>
+    <body >
+        <?php
+        $lastname="Last";
+        $firstname="First";
+        $age=18;
+        echo nl2br("PHP Wellcome exercise 2\n".$lastname." ".$firstname." ".$age);
+        ?>
+    </body>
+</html>
