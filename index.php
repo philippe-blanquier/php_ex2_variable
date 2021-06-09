63288dc41c3dd22f08e885179e5c6a91ec2c4b0b
philippe-blanquier <philippe.blanquier@gmail.com>
2021-06-09 14:11:11 +0200

    evo

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html lang="fr">
 	<head>
-        <title>PHP ex2</title>
+        <title>PHP ex1.2</title>
     </head>
     <body >
-        <?php
-        $lastname="Last";
-        $firstname="First";
-        $age=18;
-        echo nl2br("PHP Wellcome exercise 2\n".$lastname." ".$firstname." ".$age);
-        ?>
+        <p>
+            <?php
+            $lastname="Last";
+            $firstname="First";
+            $age=18;
+            echo nl2br("PHP Wellcome exercise 2\n".$lastname." ".$firstname." ".$age);
+            ?>
+        </p>
     </body>
 </html>
